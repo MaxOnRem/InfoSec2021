839895793f76a02d988629fdf50aa290a8195b8e
Max Rem <mxremez77@gmail.com>
2021-12-22 13:18:26 +0200

    pz-7 ready

---
+++ Task7.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Task7._1
+{
+    class AsymmetricEncryption
+    {
+        private static RSAParameters _publicKey, _privateKey;
+        public static void AssignNewKey()
+        {
+            //алгоритм RSA
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                _publicKey = rsa.ExportParameters(false);
+                _privateKey = rsa.ExportParameters(true);
+            }
+        }
+        public static byte[] EncryptData(byte[] dataToEncrypt)
+        {
+            byte[] cipherBytes;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(_publicKey);
+                cipherBytes = rsa.Encrypt(dataToEncrypt, true);
+            }
+            return cipherBytes;
+        }
+        public static byte[] DecryptData(byte[] dataToDecrypt)
+        {
+            byte[] plaintext;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(_privateKey);
+                plaintext = rsa.Decrypt(dataToDecrypt, true);
+            }
+            return plaintext;
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            const string original = "This message is very secret";
+            AsymmetricEncryption.AssignNewKey();
+            var encrypted = AsymmetricEncryption.EncryptData(Encoding.UTF8.GetBytes(original));
+            var decrypted = AsymmetricEncryption.DecryptData(encrypted);
+            Console.WriteLine("Original Message: " + original);
+            Console.WriteLine("Encrypted Message: " + Convert.ToBase64String(encrypted));
+            Console.WriteLine("Decrypted Message: " + Encoding.UTF8.GetString(decrypted));
+        }
+    }
+}
